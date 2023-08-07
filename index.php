e0caf4d1112f365a100890987bc0303adf0163c0
empal030110 <empal03@gmail.com>
2023-08-07 22:10:22 +0900

    add: index 페이지

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+    include 'tpl/body_tpl.php';
+    function html_body() {
+?>
+<div class="page_wrap">
+    <?php if (/* $_SESSION['member_id'] */1) { ?>
+        <div class="bg_white page">
+            <div class="header_wrap">
+                <div class="header">
+                    <p class="font_en">SeulChukSaeng</p>
+                    <!-- <p><?=$_SESSION['member_id']?>님</p> -->
+                    <p>주민규님</p>
+                </div>
+            </div>
+            <div class="index_main">
+                <div class="list_wrap match_list">
+                    <div class="list_title_wrap"><p class="list_title">매치 목록</p></div>
+                    <div class="list_main">
+                        <div>
+                            <p class="list">매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1매치 목록1</p>
+                            <p class="list">매치 목록2</p>
+                            <p class="list">매치 목록3</p>
+                            <p class="list">매치 목록4</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="list_wrap notice_list">
+                    <div class="list_title_wrap"><p class="list_title">공지사항</p></div>
+                    <div class="list_main">
+                        <div>
+                            <p class="list">공지사항1공지사항1공지사항1공지사항1공지사항1공지사항1공지사항1공지사항1공지사항1공지사항1</p>
+                            <p class="list">공지사항2</p>
+                            <p class="list">공지사항3</p>
+                            <p class="list">공지사항4</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="list_wrap budgets_list">
+                    <div class="list_title_wrap"><p class="list_title">회비 사용 내역</p></div>
+                    <div class="list_main">
+                        <div>
+                            <p class="list">회비1회비1회비1회비1회비1회비1회비1회비1회비1회비1</p>
+                            <p class="list">회비2</p>
+                            <p class="list">회비3</p>
+                            <p class="list">회비4</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php } else { ?>
+        <div class="bg_purple page">
+            <div class="txt_white txt_center login_wrap">
+                <div class="login_header">
+                    <h2 class="font_en">SeulChukSaeng</h2>
+                    <p class="header_noti">환영합니다!!<br>슬기로운 축구생활 멤버 페이지 입니다</p>
+                </div>
+                <div class="login_main">
+                    <form action="view_control/login_control" method="post">
+                        <input type="text" name="member_id" id="member_id" class="m_b_16" placeholder="아이디">
+                        <input type="password" name="member_pw" id="member_pw" class="m_b_16" placeholder="비밀번호">
+                        <button type="submit" class="login_submit">로그인</button>
+                    </form>
+                </div>
+                <div class="go_signup_wrap">
+                    <a href="/view/signup" class="go_signup">아니 아직 계정이 없다구요? 이걸 누르세요</a>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+<?php
+    }
+?>
---
+++ tpl/body_tpl.php
@@ -0,0 +1,24 @@
+<?php
+    global $_SESSION;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>슬축생</title>
+    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/style/color.css">
+    <link rel="stylesheet" href="/style/base.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Suez+One&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&family=Suez+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?=html_body();?>
+</body>
+</html>
