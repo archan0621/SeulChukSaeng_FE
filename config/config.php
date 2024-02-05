6ed23c52b2d0e319db7688e968da0f5b95242086
JJONG <joshua_0621@naver.com>
2024-02-05 14:38:16 +0900

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,14 +1,7 @@
 <?php
-$real_api = "https://sellstory.kro.kr:30621/";
-$test_api = "https://sellstory.kro.kr:30622/";
-$my_api = "";
+$my_api = "${{ secrets.API_URL }}";
 $real_mode = false;
 $is_maintenance = false; // true => 점검 O / false => 점검 X
 
-if ($real_mode) {
-    $my_api = $real_api;
-} else {
-    $my_api = $test_api;
-}
 global $my_api, $is_maintenance;
 ?>
